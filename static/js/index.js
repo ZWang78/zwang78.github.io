6cab38d6eca0b33c4569d40713003d0999c38e53
zwang78 <zwang78@mgh.harvard.edu>
2024-10-10 22:43:00 -0400

    Initial commit

--- static/js/index.js
+++ static/js/index.js
@@ -4,25 +4,23 @@ window.HELP_IMPROVE_VIDEOJS = false;
 $(document).ready(function () {
 
     // Check for click events on the navbar burger icon
-$("body").css("visibility", "hidden");
-
-    // Wait until the page fully loads (including GIFs)
     window.onload = function() {
-        // Show the content after everything is loaded
-        $("body").css("visibility", "visible");
+        // Show all GIFs after they are fully loaded
+        $('img').css('visibility', 'visible');
+    };
 
-        var options = {
-            slidesToScroll: 1,
-            slidesToShow: 1,
-            loop: true,
-            infinite: true,
-            autoplay: true,
-            autoplaySpeed: 5000,
-        }
-
-        // Initialize all div with carousel class
-        var carousels = bulmaCarousel.attach('.carousel', options);
-
-        bulmaSlider.attach();
+    var options = {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        loop: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
     }
+
+    // Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.carousel', options);
+
+    bulmaSlider.attach();
+
 })
