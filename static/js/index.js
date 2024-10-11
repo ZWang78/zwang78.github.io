41a6c2e0e3035a5b4d07d2f40e7abc598eeec5eb
zwang78 <zwang78@mgh.harvard.edu>
2024-10-10 22:03:01 -0400

    Initial commit

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,28 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     // Check for click events on the navbar burger icon
+    $('#loading-screen').show();
+
+    // Wait for all page resources to load
+    $(window).on('load', function () {
+        // Hide the loading screen once everything is loaded
+        $('#loading-screen').fadeOut("slow");
+    });
 
     var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        loop: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
     }
 
-		// Initialize all div with carousel class
+    // Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
-	
+
     bulmaSlider.attach();
 
 })
